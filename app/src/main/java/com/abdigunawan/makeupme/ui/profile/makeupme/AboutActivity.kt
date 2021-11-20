c77f3308467bc0e86b48faeeb86c7bf4312306e4
abdigunawan <abdygunawan023@gmail.com>
2021-11-21 04:00:54 +0800

    penambahan about activity

--- app/src/main/java/com/abdigunawan/makeupme/ui/profile/makeupme/AboutActivity.kt
+++ app/src/main/java/com/abdigunawan/makeupme/ui/profile/makeupme/AboutActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.abdigunawan.makeupme.R
 import kotlinx.android.synthetic.main.activity_about.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
@@ -19,18 +20,22 @@ class AboutActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener { onBackPressed() }
 
         igAbdiGunawan.setOnClickListener {
+            Toast.makeText(this, "Membuka Instagram ", Toast.LENGTH_SHORT).show()
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/_abdigunawan/"))
             startActivity(intent)
         }
         igAdella.setOnClickListener {
+            Toast.makeText(this, "Membuka Instagram ", Toast.LENGTH_SHORT).show()
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/adelladewi.cahayani/"))
             startActivity(intent)
         }
         waAbdi.setOnClickListener {
+            Toast.makeText(this, "Membuka WhatsApp ", Toast.LENGTH_SHORT).show()
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://wa.link/zx6lta"))
             startActivity(intent)
         }
         waAdella.setOnClickListener {
+            Toast.makeText(this, "Membuka WhatsApp ", Toast.LENGTH_SHORT).show()
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://wa.link/l3tdpe"))
             startActivity(intent)
         }
