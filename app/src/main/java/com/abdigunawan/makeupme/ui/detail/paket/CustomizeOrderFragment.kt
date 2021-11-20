4b1a5878c70dff55e3c25e20295dc585eceaa7c0
abdigunawan <abdygunawan023@gmail.com>
2021-11-21 05:07:01 +0800

    penambahan CustomizeOrderFragment.kt

--- app/src/main/java/com/abdigunawan/makeupme/ui/detail/paket/CustomizeOrderFragment.kt
+++ app/src/main/java/com/abdigunawan/makeupme/ui/detail/paket/CustomizeOrderFragment.kt
@@ -1,6 +1,5 @@
 package com.abdigunawan.makeupme.ui.detail.paket
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,29 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.abdigunawan.makeupme.R
-import com.abdigunawan.makeupme.ui.auth.AuthActivity
 import com.abdigunawan.makeupme.ui.detail.DetailMuaActivity
-import kotlinx.android.synthetic.main.fragment_detail_paket.*
+import kotlinx.android.synthetic.main.fragment_customize_order.*
 
-class DetailPaketFragment : Fragment() {
+class CustomizeOrderFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_paket, container, false)
+        return inflater.inflate(R.layout.fragment_customize_order, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        btnOrderNow.setOnClickListener {
+        btnNext.setOnClickListener {
             Navigation.findNavController(it)
-                .navigate(R.id.action_fragmentDetailPaket_to_fragmentCustomizeOrder, null)
+                .navigate(R.id.action_fragmentCustomizeOrder_to_fragmentPembayaran, null)
 
             (activity as DetailMuaActivity).toolbarPayment()
         }
-
     }
 }
